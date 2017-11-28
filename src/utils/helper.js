15531432e8151b260ecd137a06e81d0176cc6759
Sak <redsanji@Saks-MacBook-Pro.local>
2017-11-29 00:07:39 +0700

    convert ip

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,15 +1,3 @@
-export const plus = (x, y) => x + y;
-
-export const testObject = (field) => {
-    const obj = {
-        aaa: '111',
-        bbb: '222',
-    }
-    obj.aaa = '111'
-    obj['aaa'] = '111'
-    console.log('aaaa')
-
-}
 
 export const convertToSubnet = (mask) => {
     const subnet = [0, 0, 0, 0].map(() => {
@@ -20,4 +8,32 @@ export const convertToSubnet = (mask) => {
         return parseInt(sub.join(''), 2);
     });
     return subnet.join('.');
+} // 1 -> 128.0.0.0
+
+export const ipaddress = (ip) => {
+    const iparray = ip.split('.').map((point) => {
+        const ipp = (+point).toString(2);
+        return '0'.repeat(8-ipp.length) + ipp;
+    });
+    return iparray.join('.');
+} // 255.255.0.128 -> 11111111.11111111.00000000.10000000
+
+// export const NetworkAdress = (ip,mask) => {
+//     const address 
+// }
+
+export const Short = (ip,mask) => {
+    return ip + '/' + mask ;
+}
+
+export const BinaryID = (ip) => {
+    return ipaddress(ip).split('.').join('');
+}
+
+export const IntegerID = (ip) => {
+    return parseInt(BinaryID(ip), 2);
+}
+
+export const HexID = (ip) => {
+    return (IntegerID(ip)).toString(16);
 }
--- src/utils/helper.test.js
+++ src/utils/helper.test.js
@@ -1,11 +1,6 @@
-import { plus, convertToSubnet } from './helper';
+import { convertToSubnet, ipaddress, Short, BinaryID, IntegerID, HexID } from './helper';
 import { expect } from 'chai';
 
-describe('plus test', () => {
-    it('should return x + y', () => {
-        expect(plus(1, 2)).to.equal(3);
-    });
-})
 
 describe('convertToSubnet test', () => {
     it('should convert mask to subnet', () => {
@@ -13,3 +8,18 @@ describe('convertToSubnet test', () => {
         expect(convertToSubnet(1)).to.equal('128.0.0.0');
     })
 })
+
+describe('convert ipdecimal to ipbinary', () => {
+    it ('should be', () => {
+        expect(ipaddress('255.255.255.255')).to.equal('11111111.11111111.11111111.11111111');
+        expect(ipaddress('255.255.0.0')).to.equal('11111111.11111111.00000000.00000000');
+        expect(ipaddress('255.255.0.1')).to.equal('11111111.11111111.00000000.00000001');
+        expect(ipaddress('255.255.0.128')).to.equal('11111111.11111111.00000000.10000000');
+        
+        
+        
+    })
+})
+
+console.log(Short('192.168.0.1', '16'));
+console.log(HexID('192.168.0.1'));
